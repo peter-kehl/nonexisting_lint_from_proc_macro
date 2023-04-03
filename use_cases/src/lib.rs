3f1f29767a87c6d8f0220e52a5684eed381fcdd4
Peter Kehl <peter.kehl@gmail.com>
2023-04-02 20:54:50 -0700

    Minor

--- use_cases/src/lib.rs
+++ use_cases/src/lib.rs
@@ -6,8 +6,18 @@ macro_rules! pure_macro_rules_simply_allow {
 }
 
 fn _pure_macro_rules_reports_non_existing_lints_fine() {
-    //pure_macro_rules_simply_allow!(clippy::non_existing_lint_here_gets_reported_and_we_are_happy, {});
-    pure_macro_rules_simply_allow!(std_non_existing_lint_from_proc_macro, {});
+    // The following gets reported
+    // - only by `cargo clippy --all`
+    // - but NOT by `cargo check --all`
+    pure_macro_rules_simply_allow!(
+        clippy::non_existing_lint_here_gets_reported_and_we_are_happy,
+        {}
+    );
+    // The following gets reported by both
+    // - `cargo check --all`
+    // - `cargo clippy --all`
+    pure_macro_rules_simply_allow!(std_non_existing_lint_here_gets_reported_and_we_are_happy, {
+    });
 }
 
 fn _macro_rules_composing_through_proc_macro_doesnt_report_non_existing_lint() {
