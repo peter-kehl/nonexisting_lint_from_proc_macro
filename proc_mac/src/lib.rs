83fbd82b3986139830bfc83b1a1c95fb98dea2f3
Peter Kehl <peter.kehl@gmail.com>
2023-04-11 12:53:48 -0700

    Setting span

--- proc_mac/src/lib.rs
+++ proc_mac/src/lib.rs
@@ -1,26 +1,71 @@
-use proc_macro::TokenStream;
+#![allow(unstable_features)]
+#![feature(proc_macro_def_site)]
+#![deny(unknown_lints)]
+use proc_macro::{Delimiter, Group, Ident, Punct, Spacing, Span, TokenStream, TokenTree};
+
+// The incorrect lint name `non_existing_std_lint_from_proc_macro` (in proc macro below) doesn't get
+// reported, regardless of which `Span` we use:
+fn span() -> Span {
+    Span::call_site()
+    //Span::mixed_site()
+    //Span::def_site()
+}
+
+/// Consune, mutate (apply `f`), return.
+fn with_mut<T, F: Fn(&mut T)>(mut t: T, f: F) -> T {
+    f(&mut t);
+    t
+}
+
+/// Consume, set span to `span()`, return.
+fn with_span(t: TokenTree) -> TokenTree {
+    with_mut(t, |t| t.set_span(span()))
+}
 
 #[proc_macro]
 pub fn non_existing_std_lint_name_from_proc_macro(_: TokenStream) -> TokenStream {
-    // The same whether it's a (nonexisting) "clippy::"" lint, or a (nonexisting) lint with no
-    // prefix: NOT reported!
-    "#[allow(non_existing_std_lint_from_proc_macro)] fn _f() {}"
-        .parse()
-        .unwrap()
-}
+    /*if false {
+        //return "#[deny(unknown_lints)] #[allow(non_existing_std_lint_from_proc_macro)] fn _f() {}".parse().unwrap();
+        return "#[allow(non_existing_std_lint_from_proc_macro)] fn _f() {}"
+            .parse()
+            .unwrap();
+    }*/
 
-#[proc_macro]
-pub fn non_existing_clippy_lint_name_from_proc_macro(_: TokenStream) -> TokenStream {
-    // The same whether it's a (nonexisting) "clippy::"" lint, or a (nonexisting) lint with no
-    // prefix: NOT reported!
-    "#[allow(clippy::non_existing_clippy_lint_from_proc_macro)] fn _ff() {}"
-        .parse()
-        .unwrap()
-}
+    // If span() returns Span::call_site(), then (all?) the following `with_span` calls are most
+    // likely redundant, and some `.set_span(span())` calls are also redundant.
+    let mut hash = Punct::new('#', Spacing::Joint);
+    hash.set_span(span());
 
-#[proc_macro]
-pub fn non_existing_lint_prefix_from_proc_macro(_: TokenStream) -> TokenStream {
-    "#[allow(non_existing_lint_prefix_from_proc_macro::all)] fn _fff() {}"
-        .parse()
-        .unwrap()
+    let mut group_parens_lint_name = Group::new(
+        Delimiter::Parenthesis,
+        TokenStream::from(with_span(TokenTree::Ident(Ident::new(
+            "non_existing_std_lint_from_proc_macro",
+            span(),
+        )))),
+    );
+    group_parens_lint_name.set_span(span());
+
+    let mut group_bracket = Group::new(
+        Delimiter::Bracket,
+        TokenStream::from_iter([
+            with_span(TokenTree::Ident(Ident::new("allow", span()))),
+            with_span(TokenTree::Group(group_parens_lint_name)),
+        ]),
+    );
+    group_bracket.set_span(span());
+
+    let mut group_parens = Group::new(Delimiter::Parenthesis, TokenStream::new());
+    group_parens.set_span(span());
+
+    let mut group_braces = Group::new(Delimiter::Brace, TokenStream::new());
+    group_braces.set_span(span());
+
+    TokenStream::from_iter([
+        with_span(TokenTree::Punct(hash)),
+        with_span(TokenTree::Group(group_bracket)),
+        with_span(TokenTree::Ident(Ident::new("fn", span()))),
+        with_span(TokenTree::Ident(Ident::new("_f", span()))),
+        with_span(TokenTree::Group(group_parens)),
+        with_span(TokenTree::Group(group_braces)),
+    ])
 }
--- use_cases/src/lib.rs
+++ use_cases/src/lib.rs
@@ -1,43 +1,7 @@
-macro_rules! pure_macro_rules_simply_allow {
-    ($lint_path:path, $code:block) => {
-        #[allow($lint_path)]
-        $code
-    };
-}
-
-fn _pure_macro_rules_reports_non_existing_lints_fine() {
-    // The following gets reported
-    // - only by `cargo clippy --all`
-    // - but NOT by `cargo check --all`
-    //
-    // Ok. So be it.
-    pure_macro_rules_simply_allow!(
-        clippy::non_existing_lint_here_gets_reported_and_we_are_happy,
-        {}
-    );
-
-    // The following gets reported by both
-    // - `cargo check --all`
-    // - `cargo clippy --all`
-    pure_macro_rules_simply_allow!(std_non_existing_lint_here_gets_reported_and_we_are_happy, {
-    });
-
-    // The following gets reported by NEITHER
-    // - `cargo check --all`
-    // - `cargo clippy --all`
-    pure_macro_rules_simply_allow!(
-        rustdoc::non_existing_lint_here_gets_reported_and_we_are_happy,
-        {}
-    );
-}
+#![deny(unknown_lints)]
 
 fn _macro_rules_composing_through_proc_macro_doesnt_report_non_existing_lint() {
+    #![deny(unknown_lints)]
     // The following incorrect "standard" (prefixless) lint doesn't get reported at all.
     proc_mac::non_existing_std_lint_name_from_proc_macro!();
-
-    // The following incorrect clippy (prefixless) lint doesn't get reported at all.
-    proc_mac::non_existing_clippy_lint_name_from_proc_macro!();
-
-    // The following incorrect lint PREFIX does get reported.
-    proc_mac::non_existing_lint_prefix_from_proc_macro!();
 }
