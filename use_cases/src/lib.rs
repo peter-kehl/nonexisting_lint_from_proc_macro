2899639ea56d6da2d89385ee9199c6d6101c6036
Peter Kehl <peter.kehl@gmail.com>
2023-05-21 09:42:56 -0700

    Example for /rust-lang/rust/issues/109881 with paste: Non-existing lints DO get reported.

--- use_cases/src/lib.rs
+++ use_cases/src/lib.rs
@@ -1,7 +1,25 @@
-#![deny(unknown_lints)]
+#![forbid(unknown_lints)]
 
 fn _macro_rules_composing_through_proc_macro_doesnt_report_non_existing_lint() {
-    #![deny(unknown_lints)]
+    #![forbid(unknown_lints)]
     // The following incorrect "standard" (prefixless) lint doesn't get reported at all.
     proc_mac::non_existing_std_lint_name_from_proc_macro!();
+}
+
+fn _paste_proc_macro_does_report_non_existing_lint() {
+    #![forbid(unknown_lints)]
+    // The following incorrect "standard" (prefixless) lint DOES get reported.
+    ::paste::paste! {
+        #[allow([<non_existing_std_lint_name_from_paste>])]
+        const _C: () = ();
+    }
+}
+
+fn _paste_proc_macro_composed_does_report_non_existing_lint() {
+    #![forbid(unknown_lints)]
+    // The following incorrect "standard" (prefixless) lint DOES get reported.
+    ::paste::paste! {
+        #[allow([<non_existing_ std_ lint_ name_ composed_ with_ paste>])]
+        const _C: () = ();
+    }
 }
